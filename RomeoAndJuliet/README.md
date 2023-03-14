84544bb3b00ae25a819eedfc2b76d7200385e9dd
MarioChao <89794497+MarioChao@users.noreply.github.com>
2023-03-14 13:31:03 +0800

    Shuffle Game

---
+++ RomeoAndJuliet/README.md
@@ -0,0 +1,15 @@
+# Romeo And Juliet
+The Game of Splitting Up and Never Meeting Again
+
+## Instruction
+Run the main.cpp file.
+
+There will be 8 items shuffling in a 4x2 manner.
+The game will initially start with 1 shuffle.
+As you progress and guess correctly, the rounds will get longer and longer.
+If you guess it wrong, you will lose a try.
+Once you lose all 3 tries, you will restart from the beginning.
+
+## Credits
+Inspired by GD Level "LIMBO", especially the final wave section with the shuffling keys.
+The title "Romeo and Juliet" is a play by William Shakespeare, and it is also adapted into a musical.
---
+++ RomeoAndJuliet/main.cpp
@@ -0,0 +1,227 @@
+// Romeo and Juliet - main
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <unistd.h>
+using namespace std;
+
+const int maxItems = 10, maxRounds = 30;
+
+int goalMiles = 1e5;
+int currentMiles = 0;
+
+void instructions();
+void generateEasyShuffle(int arr[maxRounds][maxItems], int items, int rounds);
+void runShuffle(int arr[maxRounds][maxItems], int items, int rounds, string &result, string symbol, int rows);
+void printCoordinate(string arr[maxItems][maxItems], int rows, int cols, int defaultLength);
+string fillStringLength(string str, int fullLength, char fillSymbol = ' ');
+void clearScreen();
+
+int main() {
+    // Speed up
+    ios::sync_with_stdio(0),
+    cin.tie(0), cout.tie(0);
+
+    // Program
+    instructions();
+    int shuffleArr[maxRounds][maxItems];
+    int items = 8, rounds = 1;
+    int tries = 3;
+    int bestScore = 0;
+    while (true) {
+        // Generate and run shuffle
+        generateEasyShuffle(shuffleArr, items, rounds);
+        string result;
+        cout << '\n';
+        runShuffle(shuffleArr, items, rounds, result, "o", 4);
+        // User input answer
+        cout << '\n';
+        cout << " What's the answer? (enter the character of the key) \n";
+        cout << " > ";
+        string response;
+        getline(cin, response);
+        // Response
+        if (result != response) {
+            cout << "_Wrong.\n";
+            sleep(1);
+            tries--;
+            if (tries == 0) {
+                cout << " Oops! You ran out of tries!\n";
+                sleep(1);
+                if (rounds == 1) {
+                    cout << " Your final score was " << rounds << " round.\n";
+                } else {
+                    cout << " Your final score was " << rounds << " rounds.\n";
+                }
+                rounds = 1;
+                tries = 3;
+            } else {
+                if (tries == 1) {
+                    cout << " You have " << tries << " try left\n";
+                } else {
+                    cout << "You have " << tries << " tries left\n";
+                }
+            }
+        } else {
+            cout << "_Correct!\n";
+            sleep(1);
+            if (rounds == 1) {
+                cout << " Your current score is " << rounds << " round.\n";
+            } else {
+                cout << " Your current score is " << rounds << " rounds.\n";
+            }
+            if (rounds > bestScore) {
+                bestScore = rounds;
+            }
+            rounds += 2;
+        }
+        sleep(1);
+        // Continue
+        cout << '\n';
+        cout << " Do you want to continue? (y/n)\n";
+        cout << " > ";
+        getline(cin, response);
+        if (response == "y") {
+            continue;
+        } else {
+            break;
+        }
+    }
+    cout << " _Quit\n";
+    cout << " Your best score was " << bestScore << " rounds.\n\n";
+}
+
+void instructions() {
+    cout << "\n";
+    cout << " - - - - - ROMEO AND JULIET - STELLAR SHUFFLE - - - - - \n";
+    cout << " Welcome to Romeo and Juliet!\n";
+    cout << " This is a game about tracking \"quickly-shuffled\" objects!\n";
+    cout << " It is an analogy to the idea that Fate often leads to tragedies.\n";
+    cout << "\n";
+    cout << " >> Press ENTER to start <<";
+    string tmp;
+    getline(cin, tmp);
+}
+
+void generateEasyShuffle(int arr[maxRounds][maxItems], int items, int rounds) {
+    // For each round i, perform an easy shuffle on the previous round
+    srand(time(0));
+    for (int round = 0; round < rounds; round++) {
+        vector<int> unused(items);
+        for (int i = 1; i <= items; i++) unused[i - 1] = i;
+        for (int item = 0; item < items; item++) {
+            int id = rand() % unused.size();
+            arr[round][item] = unused[id];
+            swap(unused[id], unused.back());
+            unused.pop_back();
+        }
+    }
+}
+
+void runShuffle(int arr[maxRounds][maxItems], int items, int rounds, string &resultStr, string symbol, int rows) {
+    // Choose the answer
+    srand(time(0));
+    int result = rand() % items + 1;
+
+    // Initialize display array
+    int cols = items / rows + (items % rows > 0 ? 1 : 0);
+    string displayArr[maxItems][maxItems];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (cols * i + j + 1 == result) {
+                displayArr[i][j] = "\"" + symbol + "\"";
+            } else {
+                displayArr[i][j] = symbol;
+            }
+        }
+    }
+
+    // Perform shuffling
+    int defaultLength = max((int) symbol.length() + 2, 6);
+    cout << fillStringLength("", defaultLength * 1.5) << "Round 0\n";
+    printCoordinate(displayArr, rows, cols, defaultLength);
+    sleep(1);
+    for (int round = 0; round < rounds; round++) {
+        // Override display
+        clearScreen();
+        cout << fillStringLength("", defaultLength * 1.5) << "Round " << round + 1 << '\n';
+        for (int item = 0; item < items; item++) {
+            int oldPos = item;
+            int oldRow = oldPos / cols;
+            int oldCol = oldPos % cols;
+            int newPos = arr[round][item] - 1;
+            int newRow = newPos / cols;
+            int newCol = newPos % cols;
+            displayArr[oldRow][oldCol] = string(1, (char) ('A' + newRow)) + to_string(newCol + 1);
+        }
+        result = arr[round][result - 1];
+        printCoordinate(displayArr, rows, cols, defaultLength);
+        // Wait 0.7 seconds
+        usleep(1e6 * 0.7);
+    }
+
+    // Show symbol array
+    clearScreen();
+    cout << fillStringLength("", defaultLength * 1.5) << "Round End\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            displayArr[i][j] = symbol;
+        }
+    }
+    printCoordinate(displayArr, rows, cols, defaultLength);
+    usleep(1e6 * 1);
+
+    // Show character array
+    clearScreen();
+    cout << fillStringLength("", defaultLength * 1.5) << "Result\n";
+    int charId = 0;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            displayArr[i][j] = string(1, (char) ('a' + charId));
+            charId++;
+        }
+    }
+    printCoordinate(displayArr, rows, cols, defaultLength);
+    usleep(1e6 * 1);
+
+    // Result
+    int resultRow = (result - 1) / cols;
+    int resultCol = (result - 1) % cols;
+    // resultStr = string(1, (char) ('A' + resultRow) + to_string(resultCol);
+    resultStr = displayArr[resultRow][resultCol];
+}
+
+void printCoordinate(string arr[maxItems][maxItems], int rows, int cols, int defaultLength) {
+    // First row
+    for (int col = 0; col <= cols; col++) {
+        if (col == 0) {
+            cout << string(defaultLength, ' ');
+        } else {
+            cout << fillStringLength(to_string(col), defaultLength);
+        }
+    }
+    cout << '\n';
+    // Rest of the rows
+    for (int row = 0; row < rows; row++) {
+        // First column
+        string character = string(1, (char) ('A' + row));
+        cout << fillStringLength(character, defaultLength);
+        // Rest of the columns
+        for (int col = 0; col < cols; col++) {
+            cout << fillStringLength(arr[row][col], defaultLength);
+        }
+        cout << '\n';
+    }
+}
+
+string fillStringLength(string str, int fullLength, char fillSymbol) {
+    int moreFill = fullLength - str.length();
+    int leftFill = moreFill / 2;
+    return string(leftFill, fillSymbol) + str + string(moreFill - leftFill, fillSymbol);
+}
+
+void clearScreen() {
+    cout << string(1000, '\n');
+}
